f9db64e3438adcc0da4d42489cebb7a9af402d47
mbrauer <mbrauer@mazetx.com>
2023-10-30 16:23:58 +0000

    incremental

--- source/200k_vep_run/03_run_vep_w_loftee_revel_am.sh
+++ source/200k_vep_run/03_run_vep_w_loftee_revel_am.sh
@@ -4,10 +4,12 @@ mkdir -p ~/staging/vep_run/output/UKB_200K_WES
 
 parallel ~/ensembl-vep/vep \
 --input_file ~/staging/vep_run/input/UKB_200K_WES/UKB_200K_WES_vep_input_part_{} \
---fasta /home/jovyan/.vep/homo_sapiens/110_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa \
+--fasta /home/jovyan/.vep/homo_sapiens/110_GRCh38/homo_sapiens_vep_110_GRCh38.tar.gz \
+#--fasta /home/jovyan/.vep/homo_sapiens/110_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa \
 #--fasta /home/jovyan/.vep/homo_sapiens/110_GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa.gz \
 --assembly GRCh38 \
 --symbol \
+--merged \
 --max_af \
 --show_ref_allele \
 --check_ref \
