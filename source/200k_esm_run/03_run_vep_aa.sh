c0061a251b57ae8c515e58d1246ec2872c79da3a
mbrauer <mbrauer@mazetx.com>
2023-10-30 20:21:27 +0000

    finalizing scripts for 200K WES and 470K WES low MAF runs

--- source/200k_esm_run/03_run_vep_aa.sh
+++ source/200k_esm_run/03_run_vep_aa.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 parallel ~/ensembl-vep/vep \
-~/ensembl-vep/vep --input_file ~/staging/vep_run/input/UKB_200K_WES/UKB_200K_WES_vep_input_part_{} \
+--input_file ~/staging/vep_run/input/UKB_200K_WES/UKB_200K_WES_vep_input_part_{} \
 --fasta /home/jovyan/.vep/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
 --assembly GRCh38 \
 --protein \
--- source/200k_vep_run/03_run_vep_alpha_missense.sh
+++ source/200k_vep_run/03_run_vep_alpha_missense.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+mkdir -p ~/staging/vep_run/output/UKB_200K_WES
+
 parallel ~/ensembl-vep/vep \
 --input_file ~/staging/vep_run/input/UKB_200K_WES/UKB_200K_WES_vep_input_part_{} \
 --fasta /home/jovyan/.vep/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
@@ -8,5 +10,18 @@ parallel ~/ensembl-vep/vep \
 --uniprot \
 --plugin AlphaMissense,file=/home/jovyan/.vep/Plugins/AlphaMissense_hg38.tsv.gz,cols=all \
 --output_file /home/jovyan/staging/vep_run/output/UKB_200K_WES/UKB_200K_WES_vep_input_part_{} \
+--cache --offline --tab --verbose --no_stats --force_overwrite \
+::: {00..95}
+
+mkdir -p ~/staging/vep_run/output/UK_BIOBANK_470K_WES_LOW_MAF
+
+parallel ~/ensembl-vep/vep \
+--input_file ~/staging/vep_run/input/UK_BIOBANK_470K_WES_LOW_MAF/UK_BIOBANK_470K_WES_LOW_MAF_vep_input_part_{} \
+--fasta /home/jovyan/.vep/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+--assembly GRCh38 \
+--protein \
+--uniprot \
+--plugin AlphaMissense,file=/home/jovyan/.vep/Plugins/AlphaMissense_hg38.tsv.gz,cols=all \
+--output_file /home/jovyan/staging/vep_run/output/UK_BIOBANK_470K_WES_LOW_MAF/UK_BIOBANK_470K_WES_LOW_MAF_vep_input_part_{} \
 --cache --offline --tab --verbose --no_stats --force_overwrite \
 ::: {00..95}
