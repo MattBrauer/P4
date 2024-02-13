6c74f3324e55293dc654c67c9800b6501d78e26c
mbrauer <mbrauer@mazetx.com>
2024-02-13 16:17:19 +0000

    removed redundant file

--- source/200k_esm_run/03_run_vep_aa.sh
+++ source/200k_esm_run/03_run_vep_aa.sh
@@ -9,3 +9,13 @@ parallel ~/ensembl-vep/vep \
 --output_file /home/jovyan/staging/vep_run/output/UKB_200K_WES_ESM/UKB_200K_WES_vep_input_part_{} \
 --cache --offline --tab --verbose --no_stats \
 ::: {00..95}
+
+parallel ~/ensembl-vep/vep \
+--input_file ~/staging/vep_run/input/mvp_ckd_grch37/mvp_ckd_grch37_vep_input_part_{} \
+--fasta /home/jovyan/.vep/homo_sapiens/dna/Homo_sapiens.GRCh37.dna.primary_assembly.fa \
+--assembly GRCh37 \
+--protein \
+--uniprot \
+--output_file /home/jovyan/staging/vep_run/output/mvp_ckd_grch37/mvp_ckd_grch37_vep_output_part_{} \
+--cache --offline --tab --verbose --no_stats \
+::: {00..95}
